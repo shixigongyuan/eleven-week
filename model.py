790607fa7dcd03c913ecb0b554d66fa89cd9dcb5
shixigongyuan <37622975+shixigongyuan@users.noreply.github.com>
2018-08-07 20:00:33 +0800

    Update model.py

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 
 class Model():
     def __init__(self, learning_rate=0.001, batch_size=16, num_steps=32, num_words=5000, dim_embedding=128, rnn_layers=3):
-        """初始化函数
+        r"""初始化函数
 
         Parameters
         ----------
@@ -55,61 +55,57 @@ class Model():
                     'embedding', [self.num_words, self.dim_embedding])
                 tf.summary.histogram('embed', embed)
 
-            data = tf.nn.embedding_lookup(embed, self.X)
-	
-            def get_a_cell(lstm_size, keep_prob):
-                lstm = tf.nn.rnn_cell.BasicLSTMCell(lstm_size)
-                drop = tf.nn.rnn_cell.DropoutWrapper(lstm, output_keep_prob=keep_prob)
-                return drop
-
+            data = tf.nn.embedding_lookup(embed, self.X)    
+            
+        def get_a_cell(lstm_size, keep_prob):
+            lstm = tf.nn.rnn_cell.BasicLSTMCell(lstm_size)
+            drop = tf.nn.rnn_cell.DropoutWrapper(lstm, output_keep_prob=keep_prob)
+            return drop
+        
         with tf.variable_scope('rnn'):
-            ##################
-            # Your Code here
-            ##################
             cell = tf.nn.rnn_cell.MultiRNNCell(
                 [get_a_cell(self.dim_embedding, self.keep_prob) for _ in range(self.rnn_layers)]
             )
            
             self.state_tensor = cell.zero_state(self.batch_size, tf.float32)
             outputs_tensor,final_state = tf.nn.dynamic_rnn(cell, data,initial_state=self.state_tensor)
-            self.outputs_state_tensor = final_state
-			
-            # concate every time step
+            self.outputs_state_tensor = final_state                
+            
+        # concate every time step
         seq_output = tf.concat(outputs_tensor, 1)
 
         # flatten it
-        seq_output_final = tf.reshape(tf.concat(outputs_tensor, 1), [-1, self.dim_embedding])
+        seq_output_final = tf.reshape(tf.concat(outputs_tensor, 1), [-1, self.dim_embedding])   
         print('seq_output.shape: ', seq_output.shape)
         print('seq_output_final.shape: ', seq_output_final.shape)
-
+        
+        
         with tf.variable_scope('softmax'):
-            ##################
-            # Your Code here
-            ##################
             W = tf.get_variable('W', [self.dim_embedding, self.num_words])
             b = tf.get_variable('b', [self.num_words])
             
         logits = tf.matmul(seq_output_final, W) + b
 
-        tf.summary.histogram('logits', logits)
-
-        self.predictions = tf.nn.softmax(logits, name='predictions')
-
+        tf.summary.histogram('logits', logits)    
+        
+        self.predictions = tf.nn.softmax(logits, name='predictions')      
+            
         y_one_hot = tf.one_hot(self.Y, self.num_words)
         print('y_one_hot.shape: ', y_one_hot.shape)
         y_reshaped = tf.reshape(y_one_hot, logits.get_shape())
-	
-        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels =  y_reshaped,logits = logits)
-        mean, var1 = tf.nn.moments(logits, -1)
+        
+        loss = tf.nn.softmax_cross_entropy_with_logits(labels =  y_reshaped,logits = logits)
+        mean, var = tf.nn.moments(logits, -1)
         self.loss = tf.reduce_mean(loss)
         tf.summary.scalar('logits_loss', self.loss)
 
-        var_loss = tf.divide(10.0, 1.0+tf.reduce_mean(var1))
+        var_loss = tf.divide(10.0, 1.0+tf.reduce_mean(var))
         tf.summary.scalar('var_loss', var_loss)
         # 把标准差作为loss添加到最终的loss里面，避免网络每次输出的语句都是机械的重复
         self.loss = self.loss + var_loss
-        tf.summary.scalar('total_loss', self.loss)
-
+        tf.summary.scalar('total_loss', self.loss)     
+        
+        
         # gradient clip
         tvars = tf.trainable_variables()
         grads, _ = tf.clip_by_global_norm(tf.gradients(loss, tvars), 5)
@@ -118,5 +114,5 @@ class Model():
             zip(grads, tvars), global_step=self.global_step)
 
         tf.summary.scalar('loss', self.loss)
-
+        
         self.merged_summary_op = tf.summary.merge_all()
