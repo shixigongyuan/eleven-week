53a9b359c3ad1720c6b71ad02805d733088bf6f1
shixigongyuan <37622975+shixigongyuan@users.noreply.github.com>
2018-08-07 20:10:16 +0800

    Update train.py

--- train.py
+++ train.py
@@ -53,7 +53,7 @@ with tf.Session() as sess:
     for x in range(1):
         logging.debug('epoch [{0}]....'.format(x))
         state = sess.run(model.state_tensor)
-        for dl in utils.get_train_data(vocabulary, batch_size=FLAGS.batch_size, num_steps=FLAGS.num_steps):
+        for dl in utils.get_train_data(vocabulary, dictionary,batch_size=FLAGS.batch_size, num_steps=FLAGS.num_steps):
 
             ##################
             # Your Code here
